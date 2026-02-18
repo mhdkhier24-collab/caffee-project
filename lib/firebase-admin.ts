7e13f01681ac256b1b0d562b7a0b20d801b37d66
Mhd Kheir <mhdkhier24@gmail.com>
2026-02-18 06:16:49 +0200

    Remove JSON and use ENV variable

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,10 +1,11 @@
+// lib/firebase-admin.ts
 import admin from "firebase-admin";
 
 let db: admin.firestore.Firestore;
 
 export function getDb() {
     if (!admin.apps.length) {
-        // اقرأ JSON من Environment Variable مباشرة
+        // اقرأ الـ service account من Environment Variable مباشرة
         const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT!);
 
         admin.initializeApp({
